f130595ea51bc2c453f1a91c374e2c5fcf28345f
Arni7 <amirnilofari@gmail.com>
2023-06-20 12:58:59 +0330

    Add some command to hardening.sh

--- hardening.sh
+++ hardening.sh
@@ -5,4 +5,23 @@ DOMAIN=pazeleshadi.ir
 SSH_PORT=1245
 BAC_DIR=/opt/backup/file_$NOW
 
-ls -l
+if [-z $BAC_DIR]; then
+  echo "Already exist!"
+else
+  mkdir -p $BAC_DIR
+fi
+
+apt update && apt upgrade -y
+
+apt install curl vim fail2ban
+
+systemctl stop ufw
+systemctl disable ufw
+systemctl mask ufw
+
+cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
+sed -i 's/ssh port/ ssh port=''$SSH_PORT/g' /etc/fail2ban/fail2ban.local
+
+sysctemctl restart fail2ban
+systemctl enable fail2ban
+fail2ban-client status
